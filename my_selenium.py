4777beca6fee7f24a35e6624933e4d171353115a
GoldenLandForever <108108205+GoldenLandForever@users.noreply.github.com>
2022-09-12 13:57:07 +0800

    Update my_selenium.py

--- my_selenium.py
+++ my_selenium.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 
 def getURL_dir(url_dir):
     browser = webdriver.Chrome('C:\Program Files\Google\Chrome\Application\chromedriver.exe')  # 使用绝对路径打开
-
+    #使用selenium来模拟打开浏览器，速度较慢，但不会412
     browser.get(url_dir)
     cookies = browser.get_cookies()
     text = browser.page_source
